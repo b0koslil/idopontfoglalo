801a42735481e6e8a42a5515948d8ff42e250c85
2020b - Kószó Liliána <74898690+b0koslil@users.noreply.github.com>
2025-03-14 08:23:33 +0100

    Update index.js

--- idopontfoglalo/src/router/index.js
+++ idopontfoglalo/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import IdopontfoglalasView from '../views/IdopontfoglalasView.vue'
+import IdopontokView from '../views/IdopontokView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,17 +7,17 @@ const router = createRouter({
     {
       path: '/',
       name: 'idopontfoglalas',
-      component: IdopontfoglalasView,
+      component: IdopontokView,
     },
     {
-      path: '/valaszt',
-      name: 'valaszt',
+      path: '/foglalasok',
+      name: 'foglalasok',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/ValasztView.vue'),
+      component: () => import('../views/FoglalasokView.vue'),
     }
-  ],
+  ]
 })
 
 export default router
